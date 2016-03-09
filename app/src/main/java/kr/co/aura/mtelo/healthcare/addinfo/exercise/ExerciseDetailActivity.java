702b3ec8684a68386ca9336d52755cf12d510fc1
young-kchoi <chaoseye@gmail.com>
2016-03-09 11:33:09 +0900

    그래프 클릭시마다 데이터를 변경해서 표시 액션바 수정

--- app/src/main/java/kr/co/aura/mtelo/healthcare/addinfo/exercise/ExerciseDetailActivity.java
+++ app/src/main/java/kr/co/aura/mtelo/healthcare/addinfo/exercise/ExerciseDetailActivity.java
@@ -1,6 +1,5 @@
 package kr.co.aura.mtelo.healthcare.addinfo.exercise;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -8,7 +7,11 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockActivity;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
@@ -30,21 +33,25 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 
 import kr.co.aura.mtelo.healthcare.R;
+import kr.co.aura.mtelo.healthcare.preferences.CPreferences;
 import kr.co.aura.mtelo.healthcare.util.MLog;
 
 /**
  * Created by young-kchoi on 2016. 2. 24..
  */
-public class ExerciseDetailActivity extends Activity implements View.OnClickListener, OnChartGestureListener, OnChartValueSelectedListener {
+public class ExerciseDetailActivity extends SherlockActivity implements View.OnClickListener, OnChartGestureListener, OnChartValueSelectedListener {
 
-    private static final int CALORIE = 1000;
-    private static final int STEP = 1001;
-    private static final int DISTANCE = 1002;
+    private final int CALORIE = 1000;
+    private final int STEP = 1001;
+    private final int DISTANCE = 1002;
+    private final int SPEED = 1003;
+    private int mChartStatus  = CALORIE;
+
     private LineChart mChart;
     private ArrayList<ChartData> mCheatDatas = new ArrayList<ChartData>();
 
     private String mCalorie, mStep, mDistance, mSpeed, mBodyType;
-
+    private TextView mCalorieText, mStepText, mSpeedText, mBodyTypeText, mDistanceText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +59,7 @@ public class ExerciseDetailActivity extends Activity implements View.OnClickList
 
         setContentView(R.layout.exerics_detail);
 
+        init_ACtionBar();
 
         Button topBtn1 = (Button) findViewById(R.id.exercise_detail_top_tx1);
         topBtn1.setOnClickListener(this);
@@ -63,6 +71,14 @@ public class ExerciseDetailActivity extends Activity implements View.OnClickList
         topBtn3.setOnClickListener(this);
 
 
+        //하단 텍스트박스
+        mCalorieText  = (TextView) findViewById(R.id.exercise_detail_bottom_right_tx1);
+        mStepText     = (TextView) findViewById(R.id.exercise_detail_bottom_right_tx2);
+        mDistanceText = (TextView) findViewById(R.id.exercise_detail_bottom_right_tx3);
+        mSpeedText    = (TextView) findViewById(R.id.exercise_detail_bottom_right_tx4);
+        mBodyTypeText = (TextView) findViewById(R.id.exercise_detail_bottom_right_tx5);
+
+
         mChart = (LineChart) findViewById(R.id.chart1);
         mChart.setOnChartGestureListener(this);
         mChart.setOnChartValueSelectedListener(this);
@@ -70,18 +86,19 @@ public class ExerciseDetailActivity extends Activity implements View.OnClickList
         mChart.setTouchEnabled(true);    // 터치동작 지원X
         mChart.setDescription("");
 
-        mChart.setDragEnabled(true);
-        mChart.setScaleEnabled(true);
-        mChart.setPinchZoom(true);
+        mChart.setDragEnabled(false);
+        mChart.setScaleEnabled(false);
+        mChart.setPinchZoom(false);
         mChart.animateXY(3000, 3000);
 
         //오른쪽 Y축을 삭제한
         mChart.getAxisRight().setEnabled(false);
 
+
         //Y축 설정
         YAxis leftAxis = mChart.getAxisLeft();
-        leftAxis.removeAllLimitLines(); // reset all limit lines to avoid overlapping lines
-        leftAxis.setAxisMaxValue(7f);
+        leftAxis.removeAllLimitLines(); // reset all limit linesto avoid overlapping lines
+        leftAxis.setAxisMaxValue(400f);
         leftAxis.setAxisMinValue(0f);
         // limit lines are drawn behind data (and not on top)
         leftAxis.setDrawLimitLinesBehindData(false);
@@ -99,7 +116,32 @@ public class ExerciseDetailActivity extends Activity implements View.OnClickList
         xAxis.setValueFormatter(new MyValueFormatter(mCheatDatas));
 
         //데이터 설정
-        setData(mCheatDatas.size() , mCheatDatas);
+        setData(mCheatDatas.size() , mCheatDatas, mChartStatus);
+
+
+    }
+
+
+
+    private void init_ACtionBar() {
+        ActionBar mActionBar;
+
+        mActionBar = getSupportActionBar();
+        mActionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        mActionBar.setCustomView(R.layout.custom_actionbar);
+
+        String str = new CPreferences(this).getUserName();
+        ((TextView) mActionBar.getCustomView().findViewById(R.id.actionbar_center_tx1)).setText(str);        //타이틀
+
+        String date = new CPreferences(this).getTitleLastDate();
+        ((TextView) mActionBar.getCustomView().findViewById(R.id.actionbar_right_tx1)).setText(date);    //날짜
+        ((ImageButton)mActionBar.getCustomView().findViewById(R.id.actionbar_left_image1)).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
@@ -122,10 +164,10 @@ public class ExerciseDetailActivity extends Activity implements View.OnClickList
     //테스트코드
     public void testCOde(){
 
-        ChartData data1 = new ChartData("2/21", "축구",  "280", "600", "4.05");
-        ChartData data2 = new ChartData("2/28", "농구", "250" ,"700",  "5.05");
-        ChartData data3 = new ChartData("3/2", "배구", "350","500","6.05");
-        ChartData data4 = new ChartData("3/10", "축구","260", "450","3.05");
+        ChartData data1 = new ChartData("2/21", "축구",  "280", "6000", "4.05", "40");
+        ChartData data2 = new ChartData("2/28", "농구", "250" ,"7000",  "5.05" , "50");
+        ChartData data3 = new ChartData("3/2", "배구", "350","5000","6.05" , "20");
+        ChartData data4 = new ChartData("3/10", "축구","260", "4500","3.05", "40");
         mCheatDatas.add(data1);
         mCheatDatas.add(data2);
         mCheatDatas.add(data3);
@@ -162,26 +204,45 @@ public class ExerciseDetailActivity extends Activity implements View.OnClickList
     }
 
     //데이터를 차트에 추가
-    private void setData(int count, ArrayList<ChartData> list) {
+    private void setData(int count, ArrayList<ChartData> list, int status) {
+        Log.e("!!!!", "!!! setData  status "+ status +", mChartStatus "+ mChartStatus);
 
         ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
         ArrayList<String> xVals = new ArrayList<String>();
-        for (int i = 0; i < count; i++) {
-            xVals.add((i) + "");
-        }
 
         ArrayList<Entry> yVals = new ArrayList<Entry>();
-        for (int i = 0; i < count ; i++ ){
-            ChartData data=  list.get(i);
+        for (int i = 0; i < count; i++) {
+            xVals.add((i) + "카운트 " + i);
 
-            yVals.add(new Entry(Float.parseFloat(data.distance ), i));
+            ChartData data = list.get(i);
+
+            switch (status) {
+                case CALORIE:
+                    yVals.add(new Entry(Float.parseFloat(data.calorie), i));
+                    break;
+
+                case STEP:
+                    yVals.add(new Entry(Float.parseFloat(data.step), i));
+                    break;
+
+                case DISTANCE:
+                    yVals.add(new Entry(Float.parseFloat(data.distance), i));
+
+                    break;
+
+                case SPEED:
+                    yVals.add(new Entry(Float.parseFloat(data.speed), i));
+                    break;
+            }
+
+        }
+
 
 
             // create a dataset and give it a type
-            LineDataSet set1 = new LineDataSet(yVals, data.date);
+            LineDataSet set1 = new LineDataSet(yVals, "이동거리");
             // set1.setFillAlpha(110);
             // set1.setFillColor(Color.RED);
-
             // set the line to be drawn like this "- - - - - -"
             set1.enableDashedLine(10f, 5f, 0f);
             set1.enableDashedHighlightLine(10f, 5f, 0f);
@@ -192,18 +253,41 @@ public class ExerciseDetailActivity extends Activity implements View.OnClickList
             set1.setDrawCircleHole(false);
             set1.setValueTextSize(12f);
 
-            dataSets.add(set1); // add the datasets
 
+        YAxis leftAxis = mChart.getAxisLeft();
+        if (status == CALORIE) {
+            leftAxis.setAxisMaxValue(430f);
+
+        } else if (status == STEP) {
+            leftAxis.setAxisMaxValue(7500f);
+            set1.setLabel("칼로리");
+        } else if (status == DISTANCE) {
+            set1.setLabel("걸음수");
+            leftAxis.setAxisMaxValue(7f);
+            set1.setLabel("이동거리");
+        } else if (status == SPEED) {
+            leftAxis.setAxisMaxValue(60f);
+            set1.setLabel("속도");
         }
 
 
+
+        dataSets.add(set1); // add the datasets
 
         // create a data object with the datasets
         LineData data = new LineData(xVals, dataSets);
 
         // set data
         mChart.setData(data);
+
+        mChartStatus = status;
     }
+
+
+    private void changeChart(){
+
+    }
+
 
     // 차크 메소드들
     @Override
@@ -223,18 +307,25 @@ public class ExerciseDetailActivity extends Activity implements View.OnClickList
 
     @Override
     public void onChartDoubleTapped(MotionEvent me) {
-
+        Log.e("!!!!", "!!! onChartDoubleTapped "+ this.getPackageName());
     }
 
     @Override
     public void onChartSingleTapped(MotionEvent me) {
+        Log.e("!!!!", "!!! onChartSingleTapped "+ mChartStatus);
+        if(mChartStatus == CALORIE)         setData(mCheatDatas.size() , mCheatDatas , STEP);
+        else if(mChartStatus == STEP)       setData(mCheatDatas.size() , mCheatDatas , DISTANCE);
+        else if(mChartStatus == DISTANCE)   setData(mCheatDatas.size() , mCheatDatas , SPEED);
+        else if(mChartStatus == SPEED)      setData(mCheatDatas.size() , mCheatDatas , CALORIE);
 
+        mChart.invalidate();
+        mChart.animateXY(3000, 3000);
 
     }
 
     @Override
     public void onChartFling(MotionEvent me1, MotionEvent me2, float velocityX, float velocityY) {
-
+        Log.e("!!!!", "!!! onChartFling "+ this.getPackageName());
     }
 
     @Override
@@ -244,7 +335,7 @@ public class ExerciseDetailActivity extends Activity implements View.OnClickList
 
     @Override
     public void onChartTranslate(MotionEvent me, float dX, float dY) {
-
+        Log.e("!!!!", "!!! onChartTranslate "+ this.getPackageName());
     }
 
     @Override
@@ -254,12 +345,25 @@ public class ExerciseDetailActivity extends Activity implements View.OnClickList
 
     @Override
     public void onNothingSelected() {
-        Log.e("!!!!", "!!! event "+ this.getPackageName());
     }
 
 
     class ChartData {
-        private String date, exercise, calorie, step, distance;
+        private String date;
+        private String exercise;
+        private String calorie;
+        private String step;
+        private String distance;
+        private String speed;
+
+        public ChartData(String date, String exercise, String calorie, String step, String distance, String speed) {
+            this.date = date;
+            this.exercise = exercise;
+            this.calorie = calorie;
+            this.step = step;
+            this.distance = distance;
+            this.speed = speed;
+        }
 
         public ChartData(String date, String exercise, String calorie, String step, String distance) {
             this.date = date;
