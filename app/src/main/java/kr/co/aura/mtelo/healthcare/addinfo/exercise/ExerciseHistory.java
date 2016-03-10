0bbb9749829168a5431609206361459d65c8276c
young-kchoi <chaoseye@gmail.com>
2016-03-10 18:09:09 +0900

    운동 history 추가중

---
+++ app/src/main/java/kr/co/aura/mtelo/healthcare/addinfo/exercise/ExerciseHistory.java
@@ -0,0 +1,21 @@
+package kr.co.aura.mtelo.healthcare.addinfo.exercise;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+/**
+ * Created by young-kchoi on 16. 3. 10..
+ */
+public class ExerciseHistory extends SherlockActivity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+
+
+}
